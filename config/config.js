fae38d73635c22444e9fde52fb4508f96fd986bf
k4rm1 <134770238+k4rm1@users.noreply.github.com>
2024-04-18 22:54:40 +0200

    push

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  token: "MTIwMjI2NTY5ODM4NzEwNzg4MA.GtzSne.28TkPgk1jiOjmNrS1GY7TXyJjaavWr9fLMDOsk",
+  botId: "1202265698387107880",
+  buyers: [
+    "798973949189947459", "1171205236799582289", "233657223190937601"
+  ],
+  prefix: "+",
+  color: "#2f3136",
+};
